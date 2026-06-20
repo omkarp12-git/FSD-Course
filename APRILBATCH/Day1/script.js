8fb2b665ea0e847a6394f15684ab2c6a01cf2499
omkarp12-git <opawar9823@gmail.com>
2026-06-20 14:29:10 +0530

    React Js Series Started

---
+++ APRILBATCH/Day1/script.js
@@ -0,0 +1,35 @@
+// // const divElement = document.getElementById("one");
+// // divElement.innerHTML = "Welcome to FSD";
+
+// let player ={
+//     jno:14,
+//     pname:"Virat",
+//     mp:100,
+//     rs:5000,
+//     'mom-name':'Saroj',
+//     dad:100
+//     'dad-name':'Prem Nath'
+// }
+// const name = 99;
+
+// console.log('I like',player.pname);
+// console.log(`I like ${player.pname}`); // to conclude any variable in a string we use backticks and ${} to conclude the variable name.
+//                                        //  This is called template literals. It is used to make the code more readable and easier to understand.
+// console.log('pname');
+// console.log(player['mom-name']);
+// console.log(player.dad-name);
+// console.log(player['dad-name']);
+
+// console.log(player);
+// player.centuries = 85
+
+
+const player1 ={
+    pno:18,
+    pname:'Virat'
+}
+console.log(player1);
+player1.pno = 45;
+player1.pname = 'Rohit';
+console.log(player1);
+
